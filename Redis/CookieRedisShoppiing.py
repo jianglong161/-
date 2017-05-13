7b3a14756303ebf672d95c119c01c8d3708e9b60
zhanglong <jianglong161@163.com>
2017-05-13 16:18:43 +0800

    用cookie实现购物车-删除旧会话

--- Redis/CookieRedisShoppiing.py
+++ Redis/CookieRedisShoppiing.py
@@ -1,8 +1,30 @@
 import redis
+import time
 ##使用reids实现购物车
 def add_to_cart(conn,session,item,count):
     conn = redis.Redis(host='192.168.31.132 ', port=6379, db=0)
     if count<=0:
-        conn.hrem('cart:'+session,item)
+        conn.hrem('cart:'+session,item) #从购物车里面删除指定商品
     else:
-        conn.hset('cart:'+session,item,count)
+        conn.hset('cart:'+session,item,count)  #将指定产品增加到购物车里
+##
+QUIT=False
+LIMIT=1000000
+def clean_full_seesion(conn):
+    conn = redis.Redis(host='192.168.31.132 ', port=6379, db=0)
+    while not QUIT:
+        size=conn.zcard('recent:')
+        if size<=LIMIT:
+            time.sleep(1)
+            continue
+        end_index=min(size-LIMIT,100)
+        sessions=conn.zrange('recnet:',0,end_index-1)
+
+        session_key=[]
+        for sss in sessions:
+            session_key.append('viewed:'+sss)
+            session_key.append('cart:'+sss)
+
+        conn.delete(*session_key)
+        conn.hdel('longin:',*sessions)
+        conn.zrem('recent:',*sessions)
