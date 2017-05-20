68cda624dfacd0299daf878952561046168ff182
zhanglong <jianglong161@163.com>
2017-05-20 15:11:56 +0800

    记录常用日志

--- Redis/ReidisSupportApp/Log.py
+++ Redis/ReidisSupportApp/Log.py
@@ -3,6 +3,7 @@
 import logging
 import redis
 import time
+import datetime
 SEVERITY={
     logging.DEBUG:'debug',
     logging.INFO:'info',
@@ -25,3 +26,39 @@ def log_recent(conn,name,message,serverity=logging.INFO,pipe=None):
     pipe.lpush(destination,message)
     pipe.itrim(destination,0,99)
     pipe.execute()
+
+
+###
+##展示了日志并轮换最常见日志消息的方法
+def log_common(conn,name,message,servirty=logging.INFO,timeout=5):
+    conn = redis.Redis(host='192.168.31.132 ', port=6379, db=0)
+    #设置日志的安全界别
+    servirty=str(SEVERITY.get(servirty,servirty)).lower()
+    #负责存储近期常用日志的键
+    destination = 'common:%s:%s'%(name,servirty)
+    #因为程序每小时轮换一次，需要一个键来记录当前所处的小时数
+    start_key=destination+':start'
+    pipe=conn.pipeline()
+    end=time.time()+timeout
+    while time.time()<end:
+        try:
+            #对记录当前小时数的键进行监视，
+            #确保轮换操作可以正常进行
+            pipe.watch(start_key)
+            #取得当前时间
+            now = datetime.utcnow().timetuple()
+            #取得当前所处的小时数
+            hour_start=datetime(*now[:4]).isoformat()
+            exitsting=pipe.get(start_key)
+            #创建一个事物
+            pipe.multi()
+            if exitsting and exitsting <hour_start:
+                pipe.rename(destination,destination+':last')
+                pipe.rename(destination,destination+':pstart')
+            elif not exitsting:
+                pipe.set(start_key,hour_start)
+            pipe.zincrby(destination,message)
+            log_recent(pipe,name,message,start_key,pipe)
+            return
+        except redis.exception.WatchError:
+            continue
